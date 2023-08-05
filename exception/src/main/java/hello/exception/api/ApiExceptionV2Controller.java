bfc781c4972d8ee2827c4521f3b1b038c4fd413f
ymkim97 <rladudaud97@naver.com>
2023-08-06 01:56:54 +0900

    ControllerAdvice

--- exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,23 +1,24 @@
 package hello.exception.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-public class ApiExceptionController {
+public class ApiExceptionV2Controller {
 
-	@GetMapping("/api/members/{id}")
+	@GetMapping("/api2/members/{id}")
 	public MemberDto getMember(@PathVariable("id") String id) {
 
 		if (id.equals("ex")) {
@@ -25,7 +26,7 @@ public class ApiExceptionController {
 		}
 
 		if (id.equals("bad")) {
-			throw  new IllegalArgumentException("잘못된 입력 값");
+			throw new IllegalArgumentException("잘못된 입력 값");
 		}
 
 		if (id.equals("user-ex")) {
@@ -33,21 +34,6 @@ public class ApiExceptionController {
 		}
 
 		return new MemberDto(id, "hello " + id);
-	}
-
-	@GetMapping("/api/response-status-ex1")
-	public String responseStatusEx1() {
-		throw new BadRequestException();
-	}
-
-	@GetMapping("/api/response-status-ex2")
-	public String responseStatusEx2() {
-		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
-	}
-
-	@GetMapping("/api/default-handler-ex")
-	public String defaultException(@RequestParam Integer data) {
-		return "ok";
 	}
 
 	@Data
