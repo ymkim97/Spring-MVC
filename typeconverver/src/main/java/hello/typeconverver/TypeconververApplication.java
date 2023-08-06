0fa624317f885f22e9baeddffd7719cf9ba06cde
ymkim97 <rladudaud97@naver.com>
2023-08-07 01:33:34 +0900

    type converver study init

---
+++ typeconverver/src/main/java/hello/typeconverver/TypeconververApplication.java
@@ -0,0 +1,13 @@
+package hello.typeconverver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeconververApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeconververApplication.class, args);
+	}
+
+}
---
+++ typeconverver/src/test/java/hello/typeconverver/TypeconververApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.typeconverver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconververApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
